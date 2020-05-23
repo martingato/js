a6f5713ac124441d168ed3e87c314cc26e1dc0ed
Martin <example@gmail.com>
2020-05-23 08:26:20 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,99 @@
+// "use strict"
+// let number = 5;
+// const leftBorderWith = 1; // you can't change
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// {
+//     let result = 6;
+// }
+
+// console.log(result);
+
+// don't start from 5 or spec symbols, or reserve variables 'error' ..
+
+// data types:
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// let arr = ['plum.png', 6, {}, [], 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr);
+// console.log(arr[0]);
+
+//confirm - interaction with user
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+// const answer = prompt("Heve you got 18?", "18");
+// console.log(typeof(answer));
+
+// const answer = +prompt("Heve you got 18?", "18");
+// console.log(typeof(answer));
+
+// const answer = prompt("Heve you got 18?", "18");
+// console.log(answer + 5);
+
+// const answer = +prompt("Heve you got 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('what is your name?', '');
+// answers[1] = prompt('what is your surname?', '');
+// answers[2] = prompt('how old are you?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+// console.log('https://someurl.com/' + category);
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/4`);
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr, decr);
+
+// let incr = 10,
+//     decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(incr, decr);
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr++, decr--); //returns 11 and 9. But why?
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+
+
